92907197f9409d77d237abd08d965c1477471023
Amber <yy1662179@gmail.com>
2025-08-14 18:10:16 +0900

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -16,7 +16,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Amber', // 您的昵称 例如 tangly1024 // 20250814,修改by yy：true→false
   BIO: process.env.NEXT_PUBLIC_BIO || 'GrowithAmber💪', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://ambersblog-git-main-amber-s-pj.vercel.app' // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://ambersblog-9mqixbzjy-amber-s-pj.vercel.app' // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客,AmbersBlog', // 网站关键词 英文逗号隔开
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
   BEI_AN: process.env.NEXT_PUBLIC_BEI_AN || '', // 备案号 闽ICP备XXXXXX
