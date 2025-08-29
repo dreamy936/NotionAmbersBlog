9540c62c09c7dc41ae51a3be61c72a5237eb66b2
Amber <yy1662179@gmail.com>
2025-08-29 09:55:05 +0900

    Update Footer.js_Punk

--- themes/simple/components/Footer.js
+++ themes/simple/components/Footer.js
@@ -15,10 +15,17 @@ export default function Footer(props) {
     parseInt(since) < currentYear ? since + '-' + currentYear : currentYear
 
   return (
-    <footer className='relative w-full bg-black px-6 border-t'>
-      <DarkModeButton className='text-center pt-4' />
+    <footer
+      className='relative w-full px-6 border-t'
+      style={{
+        backgroundImage: "url('/topbar_punk_background.png')",
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat'
+      }}>
+  {/* <DarkModeButton className='text-center pt-4' /> */}
 
-      <div className='text-yellow-300 container mx-auto max-w-4xl py-6 md:flex flex-wrap md:flex-no-wrap md:justify-between items-center text-sm'>
+  <div className='text-yellow-300 container mx-auto max-w-4xl py-12 md:flex flex-wrap md:flex-no-wrap md:justify-between items-center text-sm'>
         <div className='text-center'>
           &copy;{`${copyrightDate}`} {siteConfig('AUTHOR')}. All rights
           reserved.
@@ -43,6 +50,32 @@ export default function Footer(props) {
             </a>
           </span>
         </div>
+      </div>
+      {/* 底部朋克尖刺SVG 蒸汽波/复古/金属/哥特暗黑风格 */}
+  <div style={{ position: 'absolute', left: 0, bottom: 0, width: '100%', height: '115px', pointerEvents: 'none', zIndex: 1 }}>
+        <svg width="100%" height="48" viewBox="0 0 1200 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <defs>
+            <linearGradient id="punkMetal" x1="0" y1="0" x2="0" y2="48" gradientUnits="userSpaceOnUse">
+              <stop stopColor="#B0B4B9" />
+              <stop offset="0.5" stopColor="#23272f" />
+              <stop offset="1" stopColor="#8B0000" />
+            </linearGradient>
+          </defs>
+          {Array.from({ length: 40 }).map((_, i) => {
+            const x = i * 30;
+            const spikeHeight = i % 2 === 0 ? 48 : 32;
+            return (
+              <polygon
+                key={i}
+                points={`${x},0 ${x+15},${spikeHeight} ${x+30},0`}
+                fill="url(#punkMetal)"
+                stroke="#222"
+                strokeWidth="2"
+                opacity="0.92"
+              />
+            );
+          })}
+        </svg>
       </div>
     </footer>
   )
