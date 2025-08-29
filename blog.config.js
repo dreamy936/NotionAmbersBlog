29906f94a53ab5c9ab57f9bdb1c7c2613e8a9c4d
Amber <yy1662179@gmail.com>
2025-08-29 11:31:21 +0900

    829Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,71 +1,57 @@
 // 注: process.env.XX是Vercel的环境变量，配置方式见：https://docs.tangly1024.com/article/how-to-config-notion-next#c4768010ae7d44609b744e79e2f9959a
 
 const BLOG = {
-  // Important page_id！！！Duplicate Template from  https://www.notion.so/tanghh/02ab3b8678004aa69e9e415905ef32a5
-  NOTION_PAGE_ID:
-    process.env.NOTION_PAGE_ID ||
-    '24fc0bb163cf80c1b9efc8ef7edaca0c',//20250829：has been changed to Amber NOTION BLOG
-  THEME: process.env.NEXT_PUBLIC_THEME || 'simple', // 
-  LANG: process.env.NEXT_PUBLIC_LANG || 'en-US', // e.g 'zh-CN','en-US'  see /lib/lang.js for more.
-  SINCE: process.env.NEXT_PUBLIC_SINCE || 2025, // e.g if leave this empty, current year will be used.
+  NOTION_PAGE_ID: process.env.NOTION_PAGE_ID || '24fc0bb163cf80c1b9efc8ef7edaca0c',
 
-  PSEUDO_STATIC: process.env.NEXT_PUBLIC_PSEUDO_STATIC || false, // 伪静态路径，开启后所有文章URL都以 .html 结尾。
-  NEXT_REVALIDATE_SECOND: process.env.NEXT_PUBLIC_REVALIDATE_SECOND || 5, // 更新缓存间隔 单位(秒)；即每个页面有5秒的纯静态期、此期间无论多少次访问都不会抓取notion数据；调大该值有助于节省Vercel资源、同时提升访问速率，但也会使文章更新有延迟。
-  APPEARANCE: process.env.NEXT_PUBLIC_APPEARANCE || 'dark', // ['light', 'dark', 'auto'], // light 日间模式 ， dark夜间模式， auto根据时间和主题自动夜间模式
-  APPEARANCE_DARK_TIME: process.env.NEXT_PUBLIC_APPEARANCE_DARK_TIME || [18, 6], // 夜间模式起至时间，false时关闭根据时间自动切换夜间模式
+  THEME: process.env.NEXT_PUBLIC_THEME || 'hexo',
+  LANG: process.env.NEXT_PUBLIC_LANG || 'en-US',
+  SINCE: process.env.NEXT_PUBLIC_SINCE || 2021,
 
-  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Amber', // 20250814,修改by yy：true→false
-  BIO: process.env.NEXT_PUBLIC_BIO || 'GrowithAmber💪', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://ambersblog-9mqixbzjy-amber-s-pj.vercel.app' // 网站地址
-  //KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客,AmbersBlog', // 网站关键词 英文逗号隔开//STOPPED USING DUE TO ERROR FROM TERMINAL
-  module.exports = {BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico'}|| '/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
-  BEI_AN: process.env.NEXT_PUBLIC_BEI_AN || '', // 备案号 闽ICP备XXXXXX
-  BEI_AN_LINK: process.env.NEXT_PUBLIC_BEI_AN_LINK || 'https://beian.miit.gov.cn/', // 备案查询链接，如果用了萌备等备案请在这里填写
+  PSEUDO_STATIC: process.env.NEXT_PUBLIC_PSEUDO_STATIC || false,
+  NEXT_REVALIDATE_SECOND: process.env.NEXT_PUBLIC_REVALIDATE_SECOND || 5,
+  APPEARANCE: process.env.NEXT_PUBLIC_APPEARANCE || 'dark',
+  APPEARANCE_DARK_TIME: process.env.NEXT_PUBLIC_APPEARANCE_DARK_TIME || [18, 6],
 
-  // RSS订阅
-  ENABLE_RSS: process.env.NEXT_PUBLIC_ENABLE_RSS || false, // 是否开启RSS订阅功能，20250814,修改by yy：true→false
+  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Amber',
+  BIO: process.env.NEXT_PUBLIC_BIO || 'GrowithAmber💪',
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://ambersblog-9mqixbzjy-amber-s-pj.vercel.app',
 
-  // 其它复杂配置
-  // 原配置文件过长，且并非所有人都会用到，故此将配置拆分到/conf/目录下, 按需找到对应文件并修改即可
-  ...require('./conf/comment.config'), // 评论插件
-  ...require('./conf/contact.config'), // 作者联系方式配置
-  ...require('./conf/post.config'), // 文章与列表配置
-  ...require('./conf/analytics.config'), // 站点访问统计
-  ...require('./conf/image.config'), // 网站图片相关配置
-  ...require('./conf/font.config'), // 网站字体
-  ...require('./conf/right-click-menu'), // 自定义右键菜单相关配置
-  ...require('./conf/code.config'), // 网站代码块样式
-  ...require('./conf/animation.config'), // 动效美化效果
-  ...require('./conf/widget.config'), // 悬浮在网页上的挂件，聊天客服、宠物挂件、音乐播放器等
-  ...require('./conf/ad.config'), // 广告营收插件
-  ...require('./conf/plugin.config'), // 其他第三方插件 algolia全文索引
+  // ✅ 正确写法：把 favicon 当成 BLOG 的一个字段
+  FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico',
 
-  // 高级用法
-  ...require('./conf/layout-map.config'), // 路由与布局映射自定义，例如自定义特定路由的页面布局
-  ...require('./conf/notion.config'), // 读取notion数据库相关的扩展配置，例如自定义表头
-  ...require('./conf/dev.config'), // 开发、调试时需要关注的配置
+  ENABLE_RSS: process.env.NEXT_PUBLIC_ENABLE_RSS || false,
 
-  // 自定义外部脚本，外部样式
-  CUSTOM_EXTERNAL_JS: [''], // e.g. ['http://xx.com/script.js','http://xx.com/script.js']
-  CUSTOM_EXTERNAL_CSS: [''], // e.g. ['http://xx.com/style.css','http://xx.com/style.css']
+  // 其它拆分配置
+  ...require('./conf/comment.config'),
+  ...require('./conf/contact.config'),
+  ...require('./conf/post.config'),
+  ...require('./conf/analytics.config'),
+  ...require('./conf/image.config'),
+  ...require('./conf/font.config'),
+  ...require('./conf/right-click-menu'),
+  ...require('./conf/code.config'),
+  ...require('./conf/animation.config'),
+  ...require('./conf/widget.config'),
+  ...require('./conf/ad.config'),
+  ...require('./conf/plugin.config'),
+  ...require('./conf/layout-map.config'),
+  ...require('./conf/notion.config'),
+  ...require('./conf/dev.config'),
 
-  // 自定义菜单
-  CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || true, // 支持Menu类型的菜单，替代了3.12版本前的Page类型
+  CUSTOM_EXTERNAL_JS: [''],
+  CUSTOM_EXTERNAL_CSS: [''],
+  CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || true,
+  CAN_COPY: process.env.NEXT_PUBLIC_CAN_COPY || true,
+  LAYOUT_SIDEBAR_REVERSE: process.env.NEXT_PUBLIC_LAYOUT_SIDEBAR_REVERSE || false,
 
-  // 文章列表相关设置
-  CAN_COPY: process.env.NEXT_PUBLIC_CAN_COPY || true, // 是否允许复制页面内容 默认允许，如果设置为false、则全栈禁止复制内容。
-
-  // 侧栏布局 是否反转(左变右,右变左) 已支持主题: hexo next medium fukasawa example
-  LAYOUT_SIDEBAR_REVERSE:
-    process.env.NEXT_PUBLIC_LAYOUT_SIDEBAR_REVERSE || false,
-
-  // 欢迎语打字效果,Hexo,Matery主题支持, 英文逗号隔开多个欢迎语。
+  // 推荐用反引号避免引号冲突
   GREETING_WORDS:
     process.env.NEXT_PUBLIC_GREETING_WORDS ||
-    'Hi,Welcome to My Blog🎉',
+    `Hi, Welcome to My Blog, こんにちは、よろしくお願いします~, 哈咯咯🎉`,
 
-  // uuid重定向至 slug
   UUID_REDIRECT: process.env.UUID_REDIRECT || false
-}
+};
 
-module.exports = BLOG
+// ✅ 只在文件最后导出
+module.exports = BLOG;
+
