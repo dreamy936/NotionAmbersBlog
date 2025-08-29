4bacf2f37982a1d2b3ad30ba39edf97b6e828307
Amber <yy1662179@gmail.com>
2025-08-29 09:54:07 +0900

    PUNK HEADER

--- themes/simple/components/Header.js
+++ themes/simple/components/Header.js
@@ -1,54 +1,82 @@
-import LazyImage from '@/components/LazyImage'
+import { BeiAnGongAn } from '@/components/BeiAnGongAn'
+import DarkModeButton from '@/components/DarkModeButton'
 import { siteConfig } from '@/lib/config'
-import SmartLink from '@/components/SmartLink'
-import CONFIG from '../config'
-import SocialButton from './SocialButton'
 
 /**
- * 网站顶部
+ * 页脚
+ * @param {*} props
  * @returns
  */
-export default function Header(props) {
-  const { siteInfo } = props
+export default function Footer(props) {
+  const d = new Date()
+  const currentYear = d.getFullYear()
+  const since = siteConfig('SINCE')
+  const copyrightDate =
+    parseInt(since) < currentYear ? since + '-' + currentYear : currentYear
 
   return (
-    <header className='text-center justify-between items-center px-6 bg-white h-80 dark:bg-black relative z-10'>
-      <div className='float-none inline-block py-12'>
-        <SmartLink href='/'>
-          {/* 可使用一张单图作为logo */}
-          <div className='flex space-x-6 justify-center'>
-            <div className='hover:rotate-45 hover:scale-125 transform duration-200 cursor-pointer justify-center items-center flex'>
-              <LazyImage
-                priority={true}
-                src={siteInfo?.icon}
-                className='rounded-full'
-                width={100}
-                height={100}
-                alt={siteConfig('AUTHOR')}
-              />
-            </div>
+    <footer
+      className='relative w-full px-6 border-t'
+      style={{
+        backgroundImage: "url('/topbar_punk_background.png')",
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat'
+      }}>
+  {/* <DarkModeButton className='text-center pt-4' /> */}
 
-            <div className='flex-col flex justify-center'>
-              <div className='text-2xl font-serif dark:text-white py-2 hover:scale-105 transform duration-200'>
-                {siteConfig('AUTHOR')}
-              </div>
-              <div
-                className='font-light dark:text-white py-2 hover:scale-105 transform duration-200 text-center'
-                dangerouslySetInnerHTML={{
-                  __html: siteConfig('SIMPLE_LOGO_DESCRIPTION', null, CONFIG)
-                }}
-              />
-            </div>
-          </div>
-        </SmartLink>
-
-        <div className='flex justify-center'>
-          <SocialButton />
+  <div className='text-yellow-300 container mx-auto max-w-4xl py-12 md:flex flex-wrap md:flex-no-wrap md:justify-between items-center text-sm'>
+        <div className='text-center'>
+          &copy;{`${copyrightDate}`} {siteConfig('AUTHOR')}. All rights
+          reserved.
         </div>
-        <div className='text-xs mt-4 text-gray-500 dark:text-gray-300'>
-          {siteConfig('DESCRIPTION')}
+        <div className='md:p-0 text-center md:text-right text-xs'>
+          {/* 右侧链接 */}
+          {/* <a href="#" className="text-black no-underline hover:underline">Privacy Policy</a> */}
+          {siteConfig('BEI_AN') && (
+            <a
+              href={siteConfig('BEI_AN_LINK')}
+              className='no-underline hover:underline ml-4'>
+              {siteConfig('BEI_AN')}
+            </a>
+          )}
+          <BeiAnGongAn />
+          <span className='no-underline ml-4'>
+            Powered by
+            <a
+              href='https://github.com/tangly1024/NotionNext'
+              className=' hover:underline'>
+              NotionNext {siteConfig('VERSION')}
+            </a>
+          </span>
         </div>
       </div>
-    </header>
+      {/* 底部朋克尖刺SVG 蒸汽波/复古/金属/哥特暗黑风格 */}
+  <div style={{ position: 'absolute', left: 0, bottom: 0, width: '100%', height: '115px', pointerEvents: 'none', zIndex: 1 }}>
+        <svg width="100%" height="48" viewBox="0 0 1200 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <defs>
+            <linearGradient id="punkMetal" x1="0" y1="0" x2="0" y2="48" gradientUnits="userSpaceOnUse">
+              <stop stopColor="#B0B4B9" />
+              <stop offset="0.5" stopColor="#23272f" />
+              <stop offset="1" stopColor="#8B0000" />
+            </linearGradient>
+          </defs>
+          {Array.from({ length: 40 }).map((_, i) => {
+            const x = i * 30;
+            const spikeHeight = i % 2 === 0 ? 48 : 32;
+            return (
+              <polygon
+                key={i}
+                points={`${x},0 ${x+15},${spikeHeight} ${x+30},0`}
+                fill="url(#punkMetal)"
+                stroke="#222"
+                strokeWidth="2"
+                opacity="0.92"
+              />
+            );
+          })}
+        </svg>
+      </div>
+    </footer>
   )
 }
